d3f3b5b451b8524bde9e15e0f3360abdaa3751d3
Soumyajit Mandal <soumyajitmandal06@gmail.com>
2026-09-06 15:08:07 +0530

    Fixed sidebar typo

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ strategy = st.sidebar.radio(
 )
 
 st.sidebar.header("3. Execution")
-sim_mode = st.siebar.radio("Simulation Mode", ["Single Cell Trajectory", "Monte Carlo Ensemble (50 cells)"])
+sim_mode = st.sidebar.radio("Simulation Mode", ["Single Cell Trajectory", "Monte Carlo Ensemble (50 cells)"])
 
 # 3. Execution Engine
 if st.sidebar.button("Run Simulation", type="primary"):
